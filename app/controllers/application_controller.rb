5bad9f2bcf5d97e68992ee17bcb2f46a2463548c
Eric Anderson <eric.anderson1@kellogg.northwestern.edu>
2024-05-18 16:53:40 +0000

    try1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def create
+    @application = Application.new
+    @application["contact_id"] = params["contact_id"]
+    @application["activity_type"] = params["activity_type"]
+    @application["note"] = params["note"]
+    @application.save
+    redirect_to "/application"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources "sessions"
+  resources "entries"
+  resources "places"
+  
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  
+  get("/", { :controller => "articles", :action => "index" })
 end
